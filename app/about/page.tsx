24e1ea95953a15fff9c8d2da4ee8c5441f175ec8
rafael <rafaelljj@gmail.com>
2024-01-12 17:15:21 -0300

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const About = () => {
+    // throw new Error('Not today')
+  return (
+    <>
+        <h1>About</h1>
+        <Link href="/">Link to Home Page</Link>
+    </>
+  )
+}
+
+export default About
+
